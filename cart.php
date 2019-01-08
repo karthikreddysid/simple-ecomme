1891c940f8e3946a29c9821c0e918cbf3b037421
karthikreddysid <karthikreddysid@gmail.com>
2019-01-08 23:17:54 +0530

    Update cart.php

--- cart.php
+++ cart.php
@@ -30,23 +30,23 @@ if(isset($_GET['add']) )
 {
   //Include database connection details
   require_once('config.php');
-  $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+  $link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
   if (!$link) {
     die("Cannot access db.");
   }
 
-  $db = mysql_select_db(DB_DATABASE);
+  $db = mysqli_select_db($link,DB_DATABASE);
   if(!$db) {
     die("Unable to select database");
   }
   $product;
-  $res = mysql_query("SELECT `tbl_product`.*,`tbl_category`.`cat_name`
+  $res = mysqli_query($link,"SELECT `tbl_product`.*,`tbl_category`.`cat_name`
             FROM `tbl_product`
             INNER JOIN `tbl_category`
             ON `tbl_product`.`cat_id`=`tbl_category`.`cat_id`
             WHERE `pd_id`=".$_GET['add']." LIMIT 1");
 
-  $product = mysql_fetch_assoc($res);
+  $product = mysqli_fetch_assoc($res);
 
   if(!isset( $_SESSION['CART']) ) $_SESSION['CART'] = array();
 
