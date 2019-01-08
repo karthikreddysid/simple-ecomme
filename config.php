880228e37a6229477b8116a00a296bb0e1e2fd1b
karthikreddysid <karthikreddysid@gmail.com>
2019-01-08 23:08:14 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	ini_set('display_errors',1);
 	error_reporting(-1);
 	define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', 'root');
+    define('DB_USER', 'ta');
+    define('DB_PASSWORD', 'password');
     define('DB_DATABASE', 'idukan');
 ?>
